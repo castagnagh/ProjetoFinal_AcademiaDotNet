992cbdf5ba692ab3e05a1c9389aedcbc5e3f933e
castagnagh <g.castagna.h@gmail.com>
2023-12-21 00:10:26 -0300

    removendo e atualizando projeto

--- ProjetoFinal/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProjetoFinal.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
